e71dc21792e448ea9fc45f4a010263d6c56cfa36
Ziad Tawfik <Ziad.muhammed.othman@gmail.com>
2023-06-16 14:43:01 -0700

    101 quote c:

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,10 +10,9 @@
 
 int main(void)
 {
-	char str[];
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i;
 
-	str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	for (i = 0; str[i] !=  '\0'; i++)
 	{
 		putchar(str[i]);

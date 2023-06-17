9fab0836c490eeec4195fb1c244c950d77bb7168
Ziad Tawfik <Ziad.muhammed.othman@gmail.com>
2023-06-16 18:19:21 -0700

    task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = '0'; x <= '7'; x++)
+	{
+		for (y = '0'; y <= '8'; y++)
+		{
+			for (z = '0'; z <= '9'; z++)
+			{
+				if (x < y && y < z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (!(x == '7' && y == '8' && z == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

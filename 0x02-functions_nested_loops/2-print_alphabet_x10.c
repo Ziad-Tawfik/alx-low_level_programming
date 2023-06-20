5abb7b53490cb17d3989cf0aa2b39e0b9741a697
Ziad Tawfik <Ziad.muhammed.othman@gmail.com>
2023-06-20 15:48:09 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - from a-z 10 times
+ * Return: Nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+    char c;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+        {
+            _putchar(c);
+        }
+        _putchar('\n');
+    }
+}
+

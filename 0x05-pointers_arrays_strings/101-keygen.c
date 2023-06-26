4fd741ae46d253aeb9cc1df5c7fa9d2846b27901
Ziad Tawfik <Ziad.muhammed.othman@gmail.com>
2023-06-26 16:05:09 -0700

    all tasks

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/**
+ * main - the program generate a 9 digits of random number as password
+ *
+ * Return: return 0.
+ */
+
+int main(void)
+{
+	int i, n;
+	unsigned long int sum;
+	char password[100];
+
+	n = i = sum = 0;
+	char *al;
+
+	al = "0123456789ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	srand(time(NULL));
+	while (sum < (2772 - 122))
+	{
+		n = rand() % 62;
+		password[i] = al[n];
+		sum = sum + password[i];
+		i++;
+	}
+	n = 2772 - sum;
+	password[i] = n;
+	password[++i] = '\0';
+	printf("%s\n", password);
+	return (0);
+}

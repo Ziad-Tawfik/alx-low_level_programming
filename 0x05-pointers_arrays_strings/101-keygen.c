a7e66b830ed5067733028eb2b6726c513f5571eb
Ziad Tawfik <Ziad.muhammed.othman@gmail.com>
2023-06-26 16:52:47 -0700

    fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,10 +14,9 @@ int main(void)
 	int i, n;
 	unsigned long int sum;
 	char password[100];
-
-	n = i = sum = 0;
 	char *al = "0123456789ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	
+	n = i = sum = 0;
 	srand(time(NULL));
 	while (sum < (2772 - 122))
 	{

b1dec857d483362ee3cf20fb905a378d699af122
Ziad Tawfik <Ziad.muhammed.othman@gmail.com>
2023-06-30 14:55:41 -0700

    gix 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,106 +1,66 @@
-#include "main.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-void _strrev(char *str);
-
 /**
- * infinite_add - a function that adds two numbers
- * @n1: a pointer to first input string
- * @n2: a pointer to second input string
- * @r: pointer to buffer where result is stored
- * @size_r: size for the buffer
+ * infinite_add - Add up two numbers stored in given char arrays
+ * @n1: The first number
+ * @n2: The second number
+ * @r: Pointer to the buffer to store result
+ * @size_r: The size of the buffer
  *
- * Return: return pointer to the result
+ * Return: 0 if buffer too small to store result, else return pointer to buffer
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	char *str1, *str2;
-	int i, m, carry, len;
+	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+	char tmp[10000];
 
-	str1 = (char *)malloc(strlen(n1) + 1);
-	str2 = (char *)malloc(strlen(n2) + 1);
-	assert(str1 != NULL);
-	assert(str2 != NULL);
-	i = m = carry = 0;
-	if (strlen(n1) > strlen(n2))
+	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+	while (n1[l1] != '\0')
+		l1++;
+	while (n2[l2] != '\0')
+		l2++;
+	if (l1 + 2 > size_r || l2 + 2 > size_r)
+		return (0);
+	l1--;
+	l2--;
+	while (i <= l1 || i <= l2)
 	{
-		strcpy(str1, n1);
-		strcpy(str2, n2);
-	}
-	else
-	{
-		strcpy(str1, n2);
-		strcpy(str2, n1);
-	}
-	len = strlen(str1);
-	if (len + 1 > size_r)
-	{
-		r = '\0';
-		return (r);
-	}
-	else
-	{
-		_strrev(str1);
-		_strrev(str2);
-		m = str1[0] - '0' + str2[0] - '0';
-		if (m >= 10)
+		num1 = num2 = 0;
+		if (i <= l1)
+			num1 = n1[l1 - i] - '0';
+		if (i <= l2 && (l2 - i) >= 0)
+			num2 = n2[l2 - i] - '0';
+		sum = num1 + num2 + carry;
+		if (sum >= 10)
+		{
 			carry = 1;
-		r[0] = m % 10 + '0';
-		for (i = 1; str2[i] != '\0'; i++)
-		{
-			m = str1[i] - '0' + str2[i] - '0' + carry;
-			if (m >= 10)
-				carry = 1;
-			else
-				carry = 0;
-			r[i] = m % 10 + '0';
-		}
-		for ( ; str1[i] != '\0'; i++)
-		{
-			m = str1[i] - '0' + carry;
-			if (m >= 10)
-				carry = 1;
-			else
-				carry = 0;
-			r[i] = m % 10 + '0';
-		}
-		if (carry == 1 && (len + 1 >= size_r))
-		{
-			r = '\0';
-			return (r);
-		}
-		else if (carry == 1 && (len + 1 < size_r))
-		{
-			r[i] = carry + '0';
-			r[++i] = '\0';
+			sum -= 10;
 		}
 		else
-		{
-			r[i] = '\0';
-		}
-		_strrev(r);
-		free(str1);
-		free(str2);
-		return (r);
-	}
-}
-
-void _strrev(char *str)
-{
-	int i, j;
-	char temp;
-
-	i = 0;
-	while (str[i] != '\0')
+			carry = 0;
+		r[i] = sum + '0';
 		i++;
-	i--;
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = str[j];
-		str[j] = str[i];
-		str[i] = temp;
+		rl++;
 	}
+	if (carry > 0)
+	{
+		r[i] = carry + '0';
+		r[i + 1] = '\0';
+	}
+	i = tmpl = 0;
+	while (i <= rl)
+	{
+		tmp[i] = r[rl - i];
+		tmpl++;
+		i++;
+	}
+	i = 0;
+	while (i < tmpl)
+	{
+		if (r[i] == '\0')
+		{
+			break;
+		}
+		r[i] = tmp[i];
+		i++;
+	}
+	return (r);
 }

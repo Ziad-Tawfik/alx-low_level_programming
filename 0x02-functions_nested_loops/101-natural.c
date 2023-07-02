b012788007281c7f829e1356ac7ae11d3f9c45a6
Ziad Tawfik <Ziad.muhammed.othman@gmail.com>
2023-07-01 18:43:10 -0700

    fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
-* main : entry point to print all natural numbers below 1024 summed
+* main - entry point to print all natural numbers below 1024 summed
+* 
 * Return: always 0
 */
 
@@ -10,7 +11,7 @@ int main(void)
 {
 int i, sum = 0;
 
-for (i = 0; i < 10; i++)
+for (i = 0; i < 1024; i++)
 {
 if ((i % 3 == 0) || (i % 5 == 0))
 {

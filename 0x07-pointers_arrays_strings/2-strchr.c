6b25e58a0790c011698f45ffe9d2122d6d9ba680
Ziad Tawfik <Ziad.muhammed.othman@gmail.com>
2023-07-03 15:37:42 -0700

    all tasks

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+* _strchr - a function that locates a character in string.
+* @s: a pointer point to string
+* @c: a constant character search for.
+*
+* Return: the pointer that the string (in success) or NULL for not found.
+*/
+
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+s++;
+}
+return (0);
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+/**
+* _strpbrk - a function that searches any set of a string
+* @s: a pointer point to the string
+*@accept: pointer to another searching string
+*
+*Return: return a pointer that point to the string after searching
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i;
+
+while (*s)
+{
+for (i = 0; accept[i] != '\0'; i++)
+{
+if (*s == accept[i])
+return (s);
+}
+s++;
+}
+return (0);
+}
